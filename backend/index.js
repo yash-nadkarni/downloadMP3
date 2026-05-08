2219e03cb24418521f5fd8b1c126438f3218e615
yvn2000 <145144435+yvn2000@users.noreply.github.com>
2026-05-08 20:26:20 +0400

    Pushing the app

---
+++ backend/index.js
@@ -0,0 +1,179 @@
+const express = require("express");
+const cors = require("cors");
+const validator = require("validator");
+const rateLimit = require("express-rate-limit");
+const { exec } = require("child_process");
+const fs = require("fs");
+const path = require("path");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000,
+    max: 20
+});
+
+app.use(limiter);
+
+const PORT = 3000;
+
+const BASE_URL = process.env.BASE_URL || `http://localhost:${PORT}`;
+
+const DOWNLOADS_DIR = path.join(__dirname, "downloads");
+
+if (!fs.existsSync(DOWNLOADS_DIR)) {
+    fs.mkdirSync(DOWNLOADS_DIR);
+}
+
+app.post("/convert", (req, res) => {
+    const { url } = req.body;
+
+    if (!url) {
+        return res.status(400).json({
+            error: "No URL provided"
+        });
+    }
+
+    if (!validator.isURL(url)) {
+        return res.status(400).json({
+            error: "Invalid URL"
+        });
+    }
+
+    if (
+        !url.includes("youtube.com") &&
+        !url.includes("youtu.be")
+    ) {
+        return res.status(400).json({
+            error: "Only YouTube URLs allowed"
+        });
+    }
+
+    const infoCommand = `yt-dlp --dump-json "${url}"`;
+
+    exec(infoCommand, (err, stdout) => {
+        if (err) {
+            return res.status(500).json({ error: "Failed to fetch video info" });
+        }
+
+        let data;
+        try {
+            data = JSON.parse(stdout);
+        } catch {
+            return res.status(500).json({ error: "Invalid metadata response" });
+        }
+
+        //filename cleaning
+        const cleanTitle = data.title
+            .replace(/[<>:"/\\|?*]+/g, "") //remove illegal chars
+            .trim();
+
+        const outputPath = path.join(DOWNLOADS_DIR, cleanTitle);
+
+        // STEP 3: Download audio using title as filename
+        //const command = `yt-dlp -x --audio-format mp3 -o "${outputPath}" "${url}"`;
+        const command =
+            `yt-dlp ` +
+            `--js-runtimes node ` +
+            `--ffmpeg-location "C:\\ffmpeg\\bin" ` +
+            `-x --audio-format mp3 ` +
+            `-o "${outputPath}.%(ext)s" ` +
+            `"${url}"`;
+
+        console.log("Running:", command);
+
+        exec(command, (error) => {
+            if (error) {
+                console.error(error);
+                return res.status(500).json({ error: "Conversion failed" });
+            }
+
+            res.json({
+                success: true,
+                title: cleanTitle,
+                download: `${BASE_URL}}/download/${cleanTitle}.mp3`
+            });
+        });
+    });
+
+
+});
+
+
+app.post("/info", (req, res) => {
+    const { url } = req.body;
+
+    const command =
+        `yt-dlp --dump-json "${url}"`;
+
+    exec(command, (error, stdout) => {
+        if (error) {
+            return res.status(500).json({
+                error: "Failed to fetch info"
+            });
+        }
+
+        try {
+            const data = JSON.parse(stdout);
+
+            res.json({
+                title: data.title,
+                duration: data.duration,
+                thumbnail: data.thumbnail,
+                uploader: data.uploader
+            });
+        }
+        catch {
+            res.status(500).json({
+                error: "Invalid metadata"
+            });
+        }
+    });
+});
+
+app.get("/download/:file", (req, res) => {
+    const filePath = path.join(DOWNLOADS_DIR, req.params.file);
+
+    if (!fs.existsSync(filePath)) {
+        return res.status(404).json({
+            error: "File not found"
+        });
+    }
+
+    //Removes the file after its been downloaded
+    res.download(filePath, () => {
+        fs.unlink(filePath, (err) => {
+            if (err) {
+                console.error(err);
+            }
+        });
+    });
+});
+
+
+//To prevent downloads from filling up indefinitely
+setInterval(() => {
+    const files = fs.readdirSync(DOWNLOADS_DIR);
+
+    files.forEach(file => {
+        const filePath = path.join(DOWNLOADS_DIR, file);
+
+        const stats = fs.statSync(filePath);
+
+        const age = Date.now() - stats.mtimeMs;
+
+        // Delete after 1 hour
+        if (age > 3600000) {
+            fs.unlinkSync(filePath);
+            console.log("Deleted:", file);
+        }
+    });
+}, 600000);
+
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
---
+++ frontend/src/App.jsx
@@ -0,0 +1,269 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  const [url, setUrl] = useState("");
+  const [info, setInfo] = useState(null);
+  const [loadingInfo, setLoadingInfo] = useState(false);
+  const [converting, setConverting] = useState(false);
+  const [downloadUrl, setDownloadUrl] = useState("");
+
+  const BASE_URL = process.env.BASE_URL || "http://localhost:3000";
+  const API = BASE_URL;
+
+  const fetchInfo = async () => {
+    setLoadingInfo(true);
+
+    try {
+      const res = await fetch(`${API}/info`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ url })
+      });
+
+      const data = await res.json();
+
+      if (data.error) {
+        alert(data.error);
+        return;
+      }
+
+      setInfo(data);
+    }
+    catch (err) {
+      console.error(err);
+      alert("Failed to fetch video info");
+    }
+    finally {
+      setLoadingInfo(false);
+    }
+  };
+
+  const convert = async () => {
+    setConverting(true);
+
+    try {
+      const res = await fetch(`${API}/convert`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ url })
+      });
+
+      const data = await res.json();
+
+      if (data.error) {
+        alert(data.error);
+        return;
+      }
+
+      console.log(data)
+
+      setDownloadUrl(data.download);
+    }
+    catch (err) {
+      console.error(err);
+      alert("Conversion failed");
+    }
+    finally {
+      setConverting(false);
+    }
+  };
+
+  return (
+    <div className="min-h-screen bg-zinc-900 text-white flex items-center justify-center p-5">
+      <div className="w-full max-w-xl bg-zinc-800 rounded-2xl p-6 shadow-xl">
+
+        <h1 className="text-3xl font-bold mb-6 text-center">
+          YouTube to MP3
+        </h1>
+
+        <input
+          type="text"
+          placeholder="Paste YouTube URL"
+          value={url}
+          onChange={(e) => setUrl(e.target.value)}
+          className="w-full p-3 rounded-lg bg-zinc-700 outline-none"
+        />
+
+        <button
+          onClick={fetchInfo}
+          disabled={loadingInfo}
+          className="w-full mt-4 bg-red-500 hover:bg-red-600 p-3 rounded-lg font-semibold"
+        >
+          {loadingInfo
+            ?
+            <div className="flex justify-center items-center">
+              <div className="w-6 h-6 border-4 border-white border-t-transparent rounded-full animate-spin"></div>
+            </div>
+            : "Fetch Video"}
+        </button>
+
+        {info && (
+          <div className="mt-6">
+
+            <img
+              src={info.thumbnail}
+              alt="thumbnail"
+              className="rounded-xl mb-4"
+            />
+
+            <h2 className="text-xl font-semibold">
+              {info.title}
+            </h2>
+
+            <p className="text-zinc-400">
+              {info.uploader}
+            </p>
+
+            <button
+              onClick={convert}
+              disabled={converting}
+              className="w-full mt-4 bg-green-500 hover:bg-green-600 p-3 rounded-lg font-semibold"
+            >
+              {converting
+                ?
+                <div className="flex justify-center items-center">
+                  <div className="w-6 h-6 border-4 border-white border-t-transparent rounded-full animate-spin"></div>
+                </div>
+                : "Convert to MP3"}
+            </button>
+          </div>
+        )}
+
+        {downloadUrl && (
+          <a
+            href={downloadUrl}
+            className="block mt-6 bg-blue-500 hover:bg-blue-600 p-3 rounded-lg text-center font-semibold"
+          >
+            Download MP3
+          </a>
+        )}
+
+      </div>
+    </div>
+  );
+
+  /*
+  return (
+    <>
+      <section id="center">
+        <div className="hero">
+          <img src={heroImg} className="base" width="170" height="179" alt="" />
+          <img src={reactLogo} className="framework" alt="React logo" />
+          <img src={viteLogo} className="vite" alt="Vite logo" />
+        </div>
+        <div>
+          <h1>Get started</h1>
+          <p>
+            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
+          </p>
+        </div>
+        <button
+          type="button"
+          className="counter"
+          onClick={() => setCount((count) => count + 1)}
+        >
+          Count is {count}
+        </button>
+      </section>
+
+      <div className="ticks"></div>
+
+      <section id="next-steps">
+        <div id="docs">
+          <svg className="icon" role="presentation" aria-hidden="true">
+            <use href="/icons.svg#documentation-icon"></use>
+          </svg>
+          <h2>Documentation</h2>
+          <p>Your questions, answered</p>
+          <ul>
+            <li>
+              <a href="https://vite.dev/" target="_blank">
+                <img className="logo" src={viteLogo} alt="" />
+                Explore Vite
+              </a>
+            </li>
+            <li>
+              <a href="https://react.dev/" target="_blank">
+                <img className="button-icon" src={reactLogo} alt="" />
+                Learn more
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div id="social">
+          <svg className="icon" role="presentation" aria-hidden="true">
+            <use href="/icons.svg#social-icon"></use>
+          </svg>
+          <h2>Connect with us</h2>
+          <p>Join the Vite community</p>
+          <ul>
+            <li>
+              <a href="https://github.com/vitejs/vite" target="_blank">
+                <svg
+                  className="button-icon"
+                  role="presentation"
+                  aria-hidden="true"
+                >
+                  <use href="/icons.svg#github-icon"></use>
+                </svg>
+                GitHub
+              </a>
+            </li>
+            <li>
+              <a href="https://chat.vite.dev/" target="_blank">
+                <svg
+                  className="button-icon"
+                  role="presentation"
+                  aria-hidden="true"
+                >
+                  <use href="/icons.svg#discord-icon"></use>
+                </svg>
+                Discord
+              </a>
+            </li>
+            <li>
+              <a href="https://x.com/vite_js" target="_blank">
+                <svg
+                  className="button-icon"
+                  role="presentation"
+                  aria-hidden="true"
+                >
+                  <use href="/icons.svg#x-icon"></use>
+                </svg>
+                X.com
+              </a>
+            </li>
+            <li>
+              <a href="https://bsky.app/profile/vite.dev" target="_blank">
+                <svg
+                  className="button-icon"
+                  role="presentation"
+                  aria-hidden="true"
+                >
+                  <use href="/icons.svg#bluesky-icon"></use>
+                </svg>
+                Bluesky
+              </a>
+            </li>
+          </ul>
+        </div>
+      </section>
+
+      <div className="ticks"></div>
+      <section id="spacer"></section>
+    </>
+  )
+  */
+}
+
+export default App
